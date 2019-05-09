8a6bfbc41c204345b1df95eeb0e1e159eed3b178
Theo Turner <theo_turner@outlook.com>
2019-05-09 12:43:46 +0100

    Update app to serve ABIs

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,14 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/', (req, res) => res.send('Hello World!'));
+app.use('/contracts', express.static(__dirname + '/contracts'));
 
 // eslint-disable-next-line no-console
 module.exports = app.listen(port, () => console.log(`Listening on port ${port}`));
